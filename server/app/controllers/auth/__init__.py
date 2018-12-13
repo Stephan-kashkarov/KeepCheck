cac5247eec1a08d721ae24c2af16cc2c8aa0efff
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-12-13 20:32:27 +1100

    did a minor rework for the auth routes

--- server/app/controllers/auth/__init__.py
+++ server/app/controllers/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint('auth', __name__, url_prefix="/auth", template_folder="../../views/auth/templates", static_folder="../../views/auth/static")
 
 from app.controllers.auth import handlers
--- server/app/controllers/auth/handlers.py
+++ server/app/controllers/auth/handlers.py
@@ -1,3 +1,4 @@
+import re
 import json
 
 from app import db, login
@@ -12,81 +13,100 @@ EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 @bp.route("/login", methods=["POST", "GET"])
 def loginController():
     if request.method == "POST":
-        data = request.json()
-        if data["header"] == "loginForm":
-            data = data["data"]
-            user = Person.query.get(data['id'])
-            if Person.checkPassword(data["pass"]):
-                login_user(user, remember=data["remember"])
-                return json.dumps({
-                    "response": "true",
-                    "error": "None"
-                })
-
+        if not request.is_json():
             return json.dumps({
                 "response": "false",
-                "error": "login"
+                "error": "No json"
             })
-        return json.dumps({
-            "response": "false",
-            "error": "invalid form"
-        })
+        else:
+            data = request.json()
+            if data["header"] == "loginForm":
+                data = data["data"]
+                user = Person.query.get(data['id'])
+                if Person.checkPassword(data["pass"]):
+                    login_user(user, remember=data["remember"])
+                    return json.dumps({
+                        "response": "true",
+                        "error": "None"
+                    })
+
+                return json.dumps({
+                    "response": "false",
+                    "error": "login"
+                })
+            return json.dumps({
+                "response": "false",
+                "error": "invalid form"
+            })
     else:
         return render_template("auth/login")
 
 
 @bp.route("/logout", methods=["POST", "GET"])
 def logoutController():
-    if current_user.is_authenticated:
+    if request.method == "POST":
         logout_user()
-        flash("User succsesfully logged out")
+        return json.dumps({
+            "response": "true",
+            "error": "None"
+        })
+    else:
+        if current_user.is_authenticated:
+            logout_user()
+            flash("User succsesfully logged out")
     return redirect(url_for("/auth/login"))
 
 
 @bp.route("/register", methods=["POST", "GET"])
 def registerController():
     if request.method == "POST":
-        data = request.json()
-        if data["header"] == "registerForm":
-            data = data["data"]
-            users = Person.query.all()
-            newUser = {
-                "username": data["username"] if "username" in data else None,
-                "email": data["email"] if "email" in data else None
-            }
-            if len(newUser["username"]) > 4 and newUser["username"] not in [x.name for x in users]:
-                newUser["username"] = True
-            if EMAIL_REGEX.match(newUser["email"]) and newUser["email"] not in [x.email for x in users]:
-                newUser["email"] = True
-            
-            if newUser["username"] and newUser["email"]:
-                newUser = Person()
-                newUser.name = data["username"]
-                newUser.email = data["email"]
-                newUser.setPass(data["password"])
-                newUser.bio = data["bio"] if "bio" in data
-                return json.dumps({
-                    "response": "true",
-                    "error": "None"
-                })
+        if not request.is_json():
+            return json.dumps({
+                "response": "false",
+                "error": "No json"
+            })
+        else:
+            data = request.json()
+            if data["header"] == "registerForm":
+                data = data["data"]
+                users = Person.query.all()
+                newUser = {
+                    "username": data["username"] if "username" in data else None,
+                    "email": data["email"] if "email" in data else None
+                }
+                if len(newUser["username"]) > 4 and newUser["username"] not in [x.name for x in users]:
+                    newUser["username"] = True
+                if EMAIL_REGEX.match(newUser["email"]) and newUser["email"] not in [x.email for x in users]:
+                    newUser["email"] = True
+                
+                if newUser["username"] and newUser["email"]:
+                    newUser = Person()
+                    newUser.name = data["username"]
+                    newUser.email = data["email"]
+                    newUser.setPass(data["password"])
+                    newUser.bio = data["bio"] if "bio" in data else None
+                    return json.dumps({
+                        "response": "true",
+                        "error": "None"
+                    })
 
-            elif not newUser["username"]:
-                return json.dumps({
-                    "response": "false",
-                    "error": "username"
-                })
+                elif not newUser["username"]:
+                    return json.dumps({
+                        "response": "false",
+                        "error": "username"
+                    })
 
-            else:
-                return json.dumps({
-                    "response": "false",
-                    "error": "email"
-                })
+                else:
+                    return json.dumps({
+                        "response": "false",
+                        "error": "email"
+                    })
 
 
-        return json.dumps({
-            "response": "false",
-            "error": "invalid form"
-        })
+            return json.dumps({
+                "response": "false",
+                "error": "invalid form"
+            })
     else:
         if current_user.is_authenticated:
             flash("You are already logged in, Log out to create new username")
